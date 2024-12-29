b585b9fd37e17e538fb8aeecd18e54e066d5bf13
Eric Max Kaplin <55173884+MaxKablaam@users.noreply.github.com>
2024-12-28 23:10:15 -0800

    WIP merging chuck example into template

--- src/my_node.cpp
+++ src/my_node.cpp
@@ -1,8 +1,17 @@
 #include "my_node.hpp"
 
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/classes/audio_stream_player.hpp>
+#include <godot_cpp/classes/audio_stream_generator_playback.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+
 
 using namespace godot;
+
+static void _print(const char *s)
+{
+	UtilityFunctions::print(s);
+}
 
 void MyNode::_bind_methods()
 {
@@ -11,23 +20,120 @@ void MyNode::_bind_methods()
 
 MyNode::MyNode()
 {
+	init_chuck();
 }
 
 MyNode::~MyNode()
 {
+	cleanup_chuck();
 }
 
 // Override built-in methods with your own logic. Make sure to declare them in the header as well!
 
 void MyNode::_ready()
 {
+	ready_chuck();
 }
 
 void MyNode::_process(double delta)
 {
+	if(process_chuck() == 1)
+	{
+		// do something
+	}
 }
 
 godot::String MyNode::hello_node()
 {
 	return "Hello GDExtension Node\n";
+}
+
+void MyNode::init_chuck()
+{
+	// ---- Init Chuck ----
+
+	// instantiate a ChucK instance
+    the_chuck = new ChucK();
+	// set some parameters: sample rate
+    the_chuck->setParam( CHUCK_PARAM_SAMPLE_RATE, 44100 );
+    // number of input channels
+    the_chuck->setParam( CHUCK_PARAM_INPUT_CHANNELS, 2 );
+    // number of output channels
+    the_chuck->setParam( CHUCK_PARAM_OUTPUT_CHANNELS, 2 );
+    // whether to halt the VM when there is no more shred running
+    the_chuck->setParam( CHUCK_PARAM_VM_HALT, TRUE );
+    // set hint so internally can advise things like async data writes etc.
+    the_chuck->setParam( CHUCK_PARAM_IS_REALTIME_AUDIO_HINT, FALSE );
+    // turn on logging to see what ChucK is up to; higher == more info
+    // chuck->setLogLevel( 3 );
+
+	// allocate audio buffers, also after parameters are set
+    alloc_global_buffers( 1024 );
+
+	// initialize ChucK, after the parameters are set
+    the_chuck->init();
+}
+
+void MyNode::ready_chuck()
+{
+	// start ChucK VM and synthesis engine
+    the_chuck->start();
+
+	// compile some ChucK code from string (this can be called on-the-fly and repeatedly)
+    if( !the_chuck->compileCode( "repeat(10) { <<< \"hello ChucK! random integer:\", Math.random2(1,100) >>>; }", "", 1 ) )
+    {
+        // got error, baillng out...
+        exit( 1 );
+    }
+}
+
+int MyNode::process_chuck()
+{
+	// keep going as long as a shred is still running
+    // (as long as CHUCK_PARAM_VM_HALT is set to TRUE)
+    if( the_chuck->vm_running() )
+    {
+        // run ChucK for the next `bufferSize`
+        // pre-condition: `input` constains any audio input
+        // post-condition: `output` constains any synthesized audio
+        the_chuck->run( g_inputBuffer, g_outputBuffer, g_bufferSize );
+		return 1;
+    }
+	else
+	{
+		return 0;
+	}
+}
+
+void MyNode::cleanup_chuck()
+{
+	// deallocate
+    cleanup_global_buffers();
+    
+    // clean up ChucK
+    CK_SAFE_DELETE( the_chuck );
+}
+
+void MyNode::alloc_global_buffers( t_CKINT bufferSize )
+{
+    // good practice to clean, in case this function is called more than once
+    cleanup_global_buffers();
+
+    // allocate audio buffers as expected by ChucK's run() function below
+    g_inputBuffer = new SAMPLE[bufferSize * the_chuck->getParamInt(CHUCK_PARAM_INPUT_CHANNELS)];
+    g_outputBuffer = new SAMPLE[bufferSize * the_chuck->getParamInt(CHUCK_PARAM_OUTPUT_CHANNELS)];
+    // zero out
+    memset( g_inputBuffer, 0, bufferSize * the_chuck->getParamInt(CHUCK_PARAM_INPUT_CHANNELS) );
+    memset( g_outputBuffer, 0, bufferSize * the_chuck->getParamInt(CHUCK_PARAM_OUTPUT_CHANNELS) );
+    // set the buffer size
+    g_bufferSize = bufferSize;
+}
+
+void MyNode::cleanup_global_buffers()
+{
+    // reclaim memory
+    CK_SAFE_DELETE_ARRAY( g_inputBuffer );
+    CK_SAFE_DELETE_ARRAY( g_outputBuffer );
+    // reset
+    g_bufferSize = 0;
 }
--- src/my_node.hpp
+++ src/my_node.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
 #include <godot_cpp/classes/node.hpp>
+#include <chuck/src/core/chuck.h>
 
 using namespace godot;
 
-class MyNode : public Node
+class MyNode : public Node //TODO: real name should be something like ChuckVM
 {
 	GDCLASS(MyNode, Node);
+private:
+	// our ChucK instance
+	ChucK * the_chuck = NULL;
+	// audio input buffer
+	SAMPLE * g_inputBuffer = NULL;
+	// audio output buffer
+	SAMPLE * g_outputBuffer = NULL;
+	// our audio buffer size
+	t_CKINT g_bufferSize = 0;
+
+	// allocate global audio buffers
+	void alloc_global_buffers( t_CKINT bufferSize );
+	// de-allocate global audio buffers
+	void cleanup_global_buffers();
 
 protected:
 	static void _bind_methods();
@@ -19,4 +34,10 @@ public:
 	void _process(double delta) override;
 
 	godot::String hello_node();
+
+	int main(int argc, char ** argv);
+	void init_chuck();
+	void ready_chuck();
+	int process_chuck();
+	void cleanup_chuck();
 };
