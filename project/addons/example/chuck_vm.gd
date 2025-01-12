e695bfb825907e032281c805bfabd22fdd245136
Eric Max Kaplin <55173884+MaxKablaam@users.noreply.github.com>
2025-01-11 17:27:02 -0800

    refactor chuck_vm.gd to work with ChuckFile resource; update export_presets.cfg to clear include_filter

--- project/addons/example/chuck_vm.gd
+++ project/addons/example/chuck_vm.gd
@@ -4,15 +4,13 @@ class_name ChuckVM extends ChuckVMManager
 
 func _ready() -> void:
 	chuck_event.connect(_on_chuck_event)
-	#run_code("repeat(10) { <<< \"hello ChucK! random integer:\", Math.random2(1,100) >>>; }")
 	run_code(chuck_file.contents)
-	#add_shred(path)
 	await get_tree().create_timer(1).timeout
 	for i in range(10):
 		set_global_float("filter_freq", randf_range(200, 500))
 		broadcast_global_event("the_nextNote")
 		await get_tree().create_timer(0.5).timeout
-	#remove_all_shreds()
+	remove_all_shreds()
 
 
 func _on_chuck_event(name: String):
